5a64f94288e3549f8f94ca8866113d3a0ee43e2c
SemenKuznetsov <semen33p@rambler.ru>
2023-09-26 21:37:37 +0500

    start version

--- src/main/java/Develop/Main.java
+++ src/main/java/Develop/Main.java
@@ -1,8 +1,21 @@
 package Develop;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("//приветствие");
+        Scanner UsersData = new Scanner(System.in);
+        String UserString = UsersData.nextLine();
+        while (true) {
+            if (UserString.equals("-help") | UserString.equals("-h") | UserString.equals("help")) {
+                System.out.println("the help code");
+                UserString = UsersData.nextLine();
+            } else if (UserString.equals("-exit")) {
+                System.out.println("the exit");
+                System.exit(0);
+            }
+        }
     }
 }
