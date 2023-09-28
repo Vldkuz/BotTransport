c5cbcef07ec6c4d40b3f479d12693f2cd4d8e99c
SemenKuznetsov <semen33p@rambler.ru>
2023-09-28 21:51:18 +0500

    text

--- src/main/java/Develop/Main.java
+++ src/main/java/Develop/Main.java
@@ -1,6 +1,7 @@
 package Develop;
 
 import java.util.Scanner;
+import java.io.PrintStream;
 
 public class Main {
 
@@ -8,14 +9,32 @@ public class Main {
         System.out.println("//приветствие");
         Scanner UsersData = new Scanner(System.in);
         String UserString = UsersData.nextLine();
+
         while (true) {
-            if (UserString.equals("-help") | UserString.equals("-h") | UserString.equals("help")) {
-                System.out.println("the help code");
-                UserString = UsersData.nextLine();
-            } else if (UserString.equals("-exit")) {
-                System.out.println("the exit");
-                System.exit(0);
+            switch (UserString) {
+                case "-help":   //help
+                case "-h":
+                case "help":
+                case "h":
+                    System.out.println("the help code");
+                    break;
+                case "-exit": //exit
+                    System.out.println("the exit. God buy)");
+                    System.exit(0);
+                    break;
+                case "-F": //fail
+                    try {
+                        PrintStream OutFile = new PrintStream("1.txt");
+                        OutFile.println("text");
+                    } catch (Exception ex) {
+                        System.err.println(ex.getMessage());
+                    }
+                    break;
+                default:
+                    System.out.println("Not have this command. try write key(-h) or white (-exit), if you want leave");
+                    break;
             }
+            UserString = UsersData.nextLine();
         }
     }
 }
