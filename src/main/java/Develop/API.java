04d629839e23be08d127398a944f52fcc6af9253
Владислав Кузнецов <v.kuznetsov@ideco.ru>
2023-10-01 19:02:31 +0500

    Added method for getCopyright

--- src/main/java/Develop/API.java
+++ src/main/java/Develop/API.java
@@ -1,5 +1,9 @@
 package Develop;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -7,39 +11,54 @@ import java.util.Map;
 import java.util.Set;
 
 public class API {
+
     private final String APIKey;
     private final String APIUrl;
 
-    public API(String APIKey, String APIUrl) { // Для работы с API требуется инициализация ключа и url.
+    public API(String APIKey,
+            String APIUrl) { // Для работы с API требуется инициализация ключа и url.
         // Добавлена валидация на ключ
-        if (APIKey.length()==0 || APIUrl.length()==0)
+        if (APIKey.length() == 0 || APIUrl.length() == 0) {
             throw new IllegalStateException("Ошибка при вводе ключа или URL API");
+        }
 
-        this.APIKey = "apikey={" + APIKey + "}"; // Костыль для формирования GET запросов, чтобы каждый раз не формировать строчку с APIKey
+        this.APIKey = APIKey;
         this.APIUrl = APIUrl;
         // APIurl - общая часть для запросов к API. Далее в каждом методе будем подставлять определенную часть для запроса
     }
 
     public void getListStation() throws Exception {
-        final String sectionStations ="/stations_list/?";
+        final String sectionStations = "/copyright/?";
         String request = getURL(sectionStations);
         URL objectUrl = new URL(request);
         HttpURLConnection urlCon = (HttpURLConnection) objectUrl.openConnection();
+        urlCon.setRequestMethod("GET");
+        urlCon.setRequestProperty("Authorization", APIKey);
+        urlCon.setReadTimeout(1000000000);
+
         short statusCode = (short) urlCon.getResponseCode();
 
-        if (statusCode >= 400 && statusCode < 500)
-            throw new Exception("Ошибка при запросе в клиентской части");
-        if (statusCode >= 500 && statusCode < 600)
-            throw new Exception("Ошибка при запросе в серверной части");
+        if (statusCode==HttpURLConnection.HTTP_OK){
+            InputStream response = urlCon.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+            StringBuilder buf = new StringBuilder();
+            String line;
+            while ((line=reader.readLine()) != null) {
+                buf.append(line).append("\n");
+            }
 
-        Map<String, List<String>> responseObj = urlCon.getHeaderFields();
-        Set<String> fields = responseObj.keySet();
+            System.out.println(buf);
+        }
+        else {
+            throw new Exception("Ошибка при запросе к API");
+        }
+
 
 
     }
 
     private String getURL(String section) {
-        return APIUrl+section+APIKey;
+        return APIUrl + section;
     }
     //
 }
