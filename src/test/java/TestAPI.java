6fdd60395e1044e7f698a71e39f182a58f6dd461
Владислав Кузнецов <v.kuznetsov@ideco.ru>
2023-10-16 12:35:00 +0500

    Added KeysToAPI

---
+++ src/test/java/TestAPI.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestAPI {
+    @Test
+    void test()
+    {
+        assertTrue(false);
+    }
+}
