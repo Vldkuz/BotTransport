1b5c003a944c8817c95c3daa79ece4fe8f3a94c9
Кузнецов Семён <semen33p@rambler.ru>
2023-11-07 13:04:19 +0500

    Added Menu TG

--- src/main/java/Develop/Telegram/bot/TGServer/Session.java
+++ src/main/java/Develop/Telegram/bot/TGServer/Session.java
@@ -2,8 +2,14 @@ package Develop.Telegram.bot.TGServer;
 
 import Develop.Telegram.bot.StateObject;
 import Develop.Telegram.bot.TelegramBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Session {
 
@@ -15,6 +21,20 @@ public class Session {
         this.chatId = chatId;
         this.text = text;
         this.telegramBot = telegramBot;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/h", "help"));
+        listofCommands.add(new BotCommand("/bs", "Расписание рейсов по станции"));
+        listofCommands.add(new BotCommand("/lot", "Список станций следования"));
+        listofCommands.add(new BotCommand("/fs", "Расписание рейсов между станциями"));
+        listofCommands.add(new BotCommand("/ns", "Список ближайших станций"));
+        listofCommands.add(new BotCommand("/nc", "Ближайший город"));
+        listofCommands.add(new BotCommand("/ci", "Информация о перевозчике"));
+        try {
+            telegramBot.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException apiException){
+            System.out.println(apiException.getMessage());
+        }
     }
 
 
@@ -34,3 +54,49 @@ public class Session {
         }
     }
 }
+
+
+
+
+
+
+
+
+/*
+package Develop.Telegram.bot.TGServer;
+
+import Develop.Telegram.bot.StateObject;
+import Develop.Telegram.bot.TelegramBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Session {
+
+    private final String chatId;
+    private final String text;
+    private final TelegramBot telegramBot;
+
+    public Session(String chatId, String text, TelegramBot telegramBot) {
+        this.chatId = chatId;
+        this.text = text;
+        this.telegramBot = telegramBot;
+    }
+
+
+    public void run(StateObject comand) {
+        TGServer tgServer = new TGServer(chatId,);
+        sendText(tgServer.run(text, comand));
+    }
+
+    private void sendText(String massege) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(massege);
+        try {
+            telegramBot.execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
+*/
