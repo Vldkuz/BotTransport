5c50b3cd92bbf4580c41c6eca573b65bdb26bfd3
Кузнецов Семён <semen33p@rambler.ru>
2023-11-18 17:09:58 +0500

    Add template in function getDataSheduleStation

--- src/main/java/Develop/Telegram/TelegramFunctions.java
+++ src/main/java/Develop/Telegram/TelegramFunctions.java
@@ -2,6 +2,8 @@ package Develop.Telegram;
 
 import Develop.Telegram.User.ParamAPI;
 
+import java.util.List;
+
 public class TelegramFunctions {
 
 
@@ -9,19 +11,19 @@ public class TelegramFunctions {
         param.setStation(request);
     }*/
 
-    public static String getDataSheduleStation(ParamAPI paramAPI) {
+    public static String getDataSheduleStation(/*ParamAPI paramAPI,*/ String stationTypeName, String title, String transportType, List<String> thread, List<String> days) {
         StringBuilder answer = new StringBuilder("");
         // Здесь нужно добавить просто шаблон, куда будет все подставляться из объекта api
-        answer.append("тип станции:\t" + paramAPI.getParamAPIShedule().getStation().getStationTypeName());
+        answer.append("тип станции:\t" +stationTypeName);
         answer.append("\n");
-        answer.append("название станции:\t" + paramAPI.getParamAPIShedule().getStation().getTitle());
+        answer.append("название станции:\t" + title);
         answer.append("\n");
-        answer.append("тип транспорта:\t" + paramAPI.getParamAPIShedule().getStation().getTransportType() + "\n" + "\n");
+        answer.append("тип транспорта:\t" + transportType + "\n" + "\n");
 
         for (int i = 0; i < 3; ++i) {
-            answer.append("рейс\t" + paramAPI.getParamAPIShedule().getSchedule().get(i).getThread().getTitle() + "\n");
-            answer.append("даты отъезда:\t" + paramAPI.getParamAPIShedule().getSchedule().get(i).getDays() + "\n");
-            answer.append("время отправления:\t" + paramAPI.getParamAPIShedule().getSchedule().get(i).getDays() + "\n");
+            answer.append("рейс\t" + /*paramAPI.getParamAPIShedule().getSchedule().get(i).getThread().getTitle()*/thread.get(i) + "\n");
+            answer.append("даты отъезда:\t" + /*paramAPI.getParamAPIShedule().getSchedule().get(i).getDays()*/days.get(i) + "\n");
+            answer.append("время отправления:\t" + /*paramAPI.getParamAPIShedule().getSchedule().get(i).getDays()*/days.get(i) + "\n");
             answer.append("\n\n");
         }
 
--- src/main/java/Develop/Telegram/User/ParamAPI.java
+++ src/main/java/Develop/Telegram/User/ParamAPI.java
@@ -3,14 +3,18 @@ package Develop.Telegram.User;
 import Develop.API.APIExceptions.HTTPClientException;
 import Develop.API.APIExceptions.ParserException;
 import Develop.API.APIExceptions.ValidationException;
+import Develop.API.APIObj.SheduleStation.Shedule;
 import Develop.API.APIObj.SheduleStation.SheduleStation;
 import Develop.API.APIServices.ParamBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParamAPI {
     private ParamBuilder param;
     private SheduleStation shedule;
 
-    public ParamAPI(String request, Session session){
+    public ParamAPI(String request, Session session) {
         param = new ParamBuilder();
         param.setStation(request);
         try {
@@ -27,4 +31,33 @@ public class ParamAPI {
     public SheduleStation getParamAPIShedule() {
         return shedule;
     }
+
+    public String getStationTypeName() {
+        return shedule.getStation().getStationTypeName();
+    }
+
+    public String getTitle() {
+        return shedule.getStation().getTitle();
+    }
+
+    public String getTransportType() {
+        return shedule.getStation().getTransportType();
+    }
+
+    public List<String> getThreadTitle() {
+        List<String> title = new ArrayList<>();
+        for (int i = 0;i< 3;++i){
+            title.add(shedule.getSchedule().get(i).getThread().getTitle());
+        }
+        return title;
+    }
+
+    public List<String> getDays() {
+        List<String> title = new ArrayList<>();
+        for (int i = 0;i< 3;++i){
+            title.add(shedule.getSchedule().get(i).getDays());
+        }
+        return title;
+    }
 }
+//paramAPI.getParamAPIShedule().getSchedule().get(i).getThread().getTitle()
